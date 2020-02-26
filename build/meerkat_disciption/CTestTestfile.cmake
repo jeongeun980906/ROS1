cf55b79ccd084dc51f08d2b9feaf3c0aff45401a
JeoungEun_Park <baro0906@korea.ac.kr>
2020-02-27 00:16:59 +0900

    0226

---
+++ build/meerkat_disciption/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jhmbabo/catkin_ws/src/meerkat_disciption
+# Build directory: /home/jhmbabo/catkin_ws/build/meerkat_disciption
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/meerkat_sim/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jhmbabo/catkin_ws/src/meerkat_sim
+# Build directory: /home/jhmbabo/catkin_ws/build/meerkat_sim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
